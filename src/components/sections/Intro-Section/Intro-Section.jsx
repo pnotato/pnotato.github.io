cb9ac981ab23704cb223dac8397a08cee071c4ea
Nicholas Chan <62458488+pnotato@users.noreply.github.com>
2025-03-27 23:49:40 -0700

    Update Intro-Section.jsx

--- src/components/sections/Intro-Section/Intro-Section.jsx
+++ src/components/sections/Intro-Section/Intro-Section.jsx
@@ -9,7 +9,7 @@ function IntroSection() {
                 <div className="sectionA-intro">
                     <h1>Who am I?</h1>
                     <p>I'm a computer science student based in Metro Vancouver. I was previously a co-op student at the <a href="https://www.bcgsc.ca/">Genome Sciences Centre</a>.</p>
-                    <p>I want to learn how technology can be used to better our healthcare system.</p>
+                    <p>I want to learn how technology can be used to improve the lives of others.</p>
                 </div>
                 <img src={Profile} className="profile-picture"/>
             </div>
